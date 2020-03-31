822a64b806cb2717e89d80dc5e59df25bce72641
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-31 13:55:27 +0530

    finished

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Himanshus App'),
+      home: MyHomePage(title: 'Himanshus Calculator'),
     );
   }
 }
